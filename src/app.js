49a9907957192d65ac6efdac5b1762fe305e7ab6
AbhayPc <abhaypc112@gmail.com>
2025-07-17 20:32:27 +0530

    solve cors issue

--- src/app.js
+++ src/app.js
@@ -5,18 +5,15 @@ import { errorHandler } from './middlewares/errorMiddleware.js';
 import studentRoutes from './routes/studentRoutes.js';
 import staffRoutes from './routes/staffRoutes.js';
 import permissionRoutes from './routes/permissionRoutes.js';
-import dotenv from 'dotenv';
 export const app = express();
-dotenv.config();
 
 app.use(express.json());
 
 
-app.use(cors({ 
-  origin:"https://sms-client-prsr.vercel.app",
-  credentials:true
-})); 
-
+app.use(cors({
+    origin:"https://sms-client-prsr.vercel.app",
+    credentials:true
+  })); 
 
 app.use('/api',authRoutes);
 app.use('/api',studentRoutes);
